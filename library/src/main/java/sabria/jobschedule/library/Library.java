d213ac45b34484e8ec1a73a21e9d27270abc6435
xiaomeixw <jpxw007@sina.com>
2016-01-27 22:14:09 +0800

    Initial commit
    Initial commit

---
+++ library/src/main/java/sabria/jobschedule/library/Library.java
@@ -0,0 +1,11 @@
+package sabria.jobschedule.library;
+
+/**
+ * Created by xiongwei , An Android project Engineer.
+ * Data : 2016-01-27  22:05
+ * Describe:
+ * Version : 1.0
+ * Open Source ：JobSchedule
+ */
+public class Library {
+}
